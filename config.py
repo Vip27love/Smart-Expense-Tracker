31c1966a21b883cb06753c4bedabdf719f942535
Vip27love <viplove.padmawar@gmail.com>
2026-07-04 18:09:24 +0530

    Ignore config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-DB_CONFIG = {
-    "host": "127.0.0.1",
-    "user": "root",
-    "password": "YOUR_PASSWORD",
-    "database": "expense_tracker",
-    "autocommit": True,
-    "connection_timeout": 30
-}
