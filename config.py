0c014be95be2494a919ef48506330d3eeb42bd49
Vip27love <155219637+Vip27love@users.noreply.github.com>
2026-07-04 17:37:25 +0530

    Update database password in config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 DB_CONFIG = {
     "host": "127.0.0.1",
     "user": "root",
-    "password": "V27@viplove",
+    "password": "YOUR_PASSWORD",
     "database": "expense_tracker",
     "autocommit": True,
     "connection_timeout": 30
